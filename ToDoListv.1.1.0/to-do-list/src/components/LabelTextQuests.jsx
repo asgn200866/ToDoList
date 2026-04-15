8f6945563883d78b0a256e1907dcc51d61bf116f
asgn200866 <homemail200866@gmail.com>
2026-04-15 21:42:56 +0300

    Debug styles.

--- ToDoListv.1.1.0/to-do-list/src/components/LabelTextQuests.jsx
+++ ToDoListv.1.1.0/to-do-list/src/components/LabelTextQuests.jsx
@@ -1,4 +1,5 @@
 import { ColumnNodeadline } from "./ColumnNodeadline";
+import mainPhoto from "../galery/mainphoto.jpg";
 import { ColumnTask } from "./ColumnTask";
 import "./LabelTextQuest.css";
 
@@ -7,7 +8,7 @@ export function LabelTextQuest() {
     <section className="label-text-quests" aria-label="Поля ввода задач">
       <ColumnTask />
       <div className="main-photo">
-        <img src="gallery/mainphoto.jpg" alt="Главное фото" />
+        <img src={mainPhoto} alt="Главное фото" />
       </div>
       <ColumnNodeadline />
     </section>
